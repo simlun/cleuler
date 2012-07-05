a94efb6952855e90557e5d0f1eac38439b228730
Simon Lundmark <simon.lundmark@gmail.com>
2012-07-05 20:55:26 +0200

    Initial commit, tests are greeeen!

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject euler001 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.stuartsierra/lazytest "1.2.3"]])
---
+++ test/euler001/core_test.clj
@@ -0,0 +1,7 @@
+(ns euler001.core-test
+  (:use clojure.test
+        euler001.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
