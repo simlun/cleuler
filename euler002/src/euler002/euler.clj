6cf2b6aa8af94b3df82ea81aeb2251155066fc1c
Simon Lundmark <simon.lundmark@gmail.com>
2012-07-08 00:15:29 +0200

    Cleanup

--- euler002/src/euler002/euler.clj
+++ euler002/src/euler002/euler.clj
@@ -14,6 +14,7 @@
     0
     (fib-fast (bigint 1) (bigint 0) (bigint 1) (bigint n))))
 
+; One solution
 (defn find-euler-fibs
   ([x]   (take-while #(< % x) (find-euler-fibs 0 x)))
   ([a x] (cons (fib a) (lazy-seq (find-euler-fibs (inc a) x)))))
@@ -21,11 +22,16 @@
 (defn sum-even-fibs-less-than 
   [x] (reduce + (filter even? (find-euler-fibs x))))
 
-(defn sum-even-fibs-less-than'
-  [max-fib]
-  (reduce + (for [n (range)
+; Another solution
+(defn- even-fibs-less-than
+ [max-fib]
+  (for [n (range)
       :let [f (fib n)]
       :when (even? f)
       :while (< f max-fib)]
-  f)))
+  f))
+
+(defn sum-even-fibs-less-than'
+  [max-fib]
+  (reduce + (even-fibs-less-than max-fib)))
 
