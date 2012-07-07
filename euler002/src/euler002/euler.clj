d0f047d30489189fd3858a2057e61365ddeb3328
Simon Lundmark <simon.lundmark@gmail.com>
2012-07-07 21:31:22 +0200

    Makes it work with fib numbers > 92, now coerces the numbers to BigInt.

--- euler002/src/euler002/euler.clj
+++ euler002/src/euler002/euler.clj
@@ -1,12 +1,18 @@
 (ns euler002.euler)
 
-(defn fib-fast 
+(defn- fib-fast 
   [i a b limit]
-  (if (< i limit) (fib-fast (inc i) b (+ a b) limit) b))
+  (if 
+    (< i limit)
+    (fib-fast (inc i) b (+ a b) limit)
+    b))
 
 (defn fib 
   [n]
-  (if (= n 0) 0 (fib-fast 1 0 1 n)))
+  (if
+    (= n 0)
+    0
+    (fib-fast (bigint 1) (bigint 0) (bigint 1) (bigint n))))
 
 (defn sum-even-fib [x] 0)
 
--- euler002/test/euler002/euler_test.clj
+++ euler002/test/euler002/euler_test.clj
@@ -8,7 +8,11 @@
   (fib 2) => 1
   (fib 3) => 2
   (fib 4) => 3
-  (fib 12) => 144)
+  (fib 12) => 144) 
+
+(fact "fib can handle even bigint-large numbers"
+  (fib 92) => 7540113804746346429 
+  (fib 93) => 12200160415121876738) 
 
 (future-fact
   (sum-even-fib 189) => 188)
