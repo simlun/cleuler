67393a287875b3e3a30415b5e1b1f19702486836
Petter Eriksson <periksson@splunk.com>
2012-07-08 22:59:18 +0200

    Finds factor to a value from a list.

--- euler003/src/cleuler/euler003.clj
+++ euler003/src/cleuler/euler003.clj
@@ -1,5 +1,21 @@
 (ns cleuler.euler003
       (:use [clojure.math.numeric-tower :only [floor sqrt]]))
 
-(defn possible-prime-factors [i]
-                        (cons 2 (range 1 (floor (sqrt i)) 2)))
+(defn two+odd-numbers [i] 
+  (cons 2 (range 3 (floor (sqrt i)) 2)))
+
+(defn find-factor-to-value-in-list [value n] 
+  (if (empty? n) 
+    nil
+    (let [x (first n)]
+      (if (= 0 (mod value x)) 
+        {:prime x :optimus (unchecked-divide-int value x)} 
+        (recur value (rest n))))))
+
+(defn finds-factor [i] 
+  (find-factor-to-value-in-list i (two+odd-numbers i)))
+
+(defn primes [i]
+  (let [prime (finds-factor i)] 
+    ))
+
--- euler003/test/cleuler/euler003_test.clj
+++ euler003/test/cleuler/euler003_test.clj
@@ -3,6 +3,18 @@
         cleuler.euler003))
 
 (fact 
-  (take 5 (possible-prime-factors 900)) => '(2 1 3 5 7))
+  (two+odd-numbers 100) => '(2 3 5 7 9))
 
+(fact
+  (find-factor-to-value-in-list 9 '(2 3 4)) => {:prime 3 :optimus 3}
+  (find-factor-to-value-in-list 9 '()) => nil)
 
+(fact
+  (finds-factor 24) => {:prime 2 :optimus 12}
+  (finds-factor 12) => {:prime 2 :optimus 6}
+  (finds-factor 6) => {:prime 2 :optimus 3}
+  (finds-factor 3) => nil 
+  (finds-factor 1) => nil)
+
+(future-fact 
+  (primes 13195) => '(5 7 13 29))
